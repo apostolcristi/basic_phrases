707e5223caf87244807774b9ca55ae04af0263fb
apostolcristi <apostol.cristi@yahoo.com>
2022-04-06 18:50:07 +0300

    Uploaded the music files

--- lib/mainPage/main.dart
+++ lib/mainPage/main.dart
@@ -97,7 +97,8 @@ class BasicPhraseAppScreen extends State<MainApplicationStateful> {
                         borderRadius: BorderRadius.circular(20),
                       ),
                     ),
-                    backgroundColor: MaterialStateProperty.all(Colors.transparent),
+                    backgroundColor:
+                        MaterialStateProperty.all(Colors.transparent),
                     shadowColor: MaterialStateProperty.all(Colors.transparent),
                   ),
                   onPressed: () {
